ddaf55179fc5ebb139d9986fd7449a323227ecee
Gabriel Enrico de Oliveira Silva <geos.enrico89@gmail.com>
2026-06-01 15:46:32 -0300

    feature: rotas criadas entre home e sobre mais início da página sobre

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import TestConnection from './components/Database/testConnection';
 import Home from './components/Home/Home';
+import About from './components/About/About';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <>
-      <Home />
       <TestConnection />
+
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,52 @@
 import "./About.css";
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+function About() {
+    return (
+        <div className="about">
+            {/* <img className="particulas" src="/images/home/particulas.png" alt="partículas" /> */}
 
-function About(){
-    return(
+            <header className="cabecalho">
+                <img className="cabecalho-img" src="/images/icons/logobranca.svg" alt="logo mars" />
 
-        
-        {/**
+                <nav className="cabecalho-txt">
+                    <Link className="cabecalho-txt-it" to="/">INÍCIO |</Link>
+                    <Link className="cabecalho-txt-it" to="/sobre">SOBRE A MARS |</Link>
+                    <Link className="cabecalho-txt-it" to="/contatos">CONTATOS |</Link>
+                    <Link className="cabecalho-txt-it" to="/adquira-a-sua">ADQUIRA A SUA |</Link>
+                </nav>
+            </header>
+
+            <hr />
+
+            <section className="hero-services">
+                <p className="hero-eyebrow">Sobre Nós:</p>
+                <h1 className="hero-headline">
+                    Da seleção do malte<br />
+                    ao<img className="hero-icon" src="/images/about/prancheta_forest.png" alt="" aria-hidden="true" /> último gole.
+                </h1>
+            </section>
+
+
+            <section className="sobre-historia">
+                <p>
+                    Acreditamos que uma cerveja artesanal de verdade não deve apenas fazer parte do momento,
+                    ela deve marcar a memória. Com a seleção rigorosa de maltes nobres,
+                    lúpulos selecionados e um processo de fermentação artesanal meticuloso,
+                    criamos receitas únicas para quem busca mais do que um gole. Porque em um mundo cheio de sabores comuns,
+                    a Mars foi feita para se destacar e ser inesquecível.
+                </p>
+            </section>
+
+            <hr></hr>
+        </div>
+
+    );
+}
+
+export default About;
+
+{/**
          * <!DOCTYPE html>
                 <html lang="pt-br">
                 <head>
@@ -42,7 +84,3 @@ function About(){
                 </body>
             </html>
          */}
-    );
-}
-
-export default About;
